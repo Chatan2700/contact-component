3635f679b4485c7df4009cac136f5079c082f45a
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-25 16:05:03 -0700

    startIng point

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Contact from './components/Contact'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="contacts">
+          <Contact />
+          <Contact />
+          <Contact />
+          <Contact />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
