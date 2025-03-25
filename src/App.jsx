9036bd7a3a55d6b2129c413b37aa25191c3363f0
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-25 16:23:55 -0700

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,30 @@ function App() {
     <>
       <div className="container">
         <div className="contacts">
-          <Contact />
-          <Contact />
-          <Contact />
-          <Contact />
+          <Contact
+            img="./images/mr-whiskerson.png"
+            name='Mr. Whiskerson'
+            phone='(212) 555-1234'
+            email='mr.whiskaz@catnap.meow'
+          />
+          <Contact
+            img="./images/fluffykins.png"
+            name='Fluffykins'
+            phone='(212) 555-2345'
+            email='fluff@me.com'
+          />
+          <Contact
+            img="./images/felix.png"
+            name='Felix'
+            phone='(212) 555-4567'
+            email='thecat@hotmail.com'
+          />
+          <Contact
+            img="./images/pumpkin.png"
+            name='Pumpkin'
+            phone='(0800) CAT KING'
+            email='pumpkin@scrimba.com'
+          />
         </div>
       </div>
     </>
